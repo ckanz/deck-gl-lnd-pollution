4c771a81de79bc89e141a6064a59cd69c02f4630
Clemens Anzmann <clemens.anzmann@googlemail.com>
2021-06-01 14:00:25 +0100

    add screengrid layer vis

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DeckGL from '@deck.gl/react'
 import { render } from 'react-dom'
 import { StaticMap } from 'react-map-gl'
-import { HeatmapLayer } from '@deck.gl/aggregation-layers'
+import { HeatmapLayer, ScreenGridLayer } from '@deck.gl/aggregation-layers'
 import { csv } from 'd3-request'
 import { mapboxToken } from './creds.js'
 import csvFile from './data/array.csv'
@@ -41,12 +41,34 @@ const Heatmap =({ data }) => <DeckGL
   />
 </DeckGL>
 
+const ScreenGrid =({ data }) => <DeckGL
+  layers={[new ScreenGridLayer({
+      id: 'grid',
+      data,
+      opacity: 0.8,
+      getPosition: d => [Number(d.lon), Number(d.lat)],
+      getWeight: d => Number(d.conc),
+      cellSizePixels: 10,
+      gpuAggregation: true,
+      aggregation: 'MEAN'
+  })]}
+  initialViewState={INITIAL_VIEW_STATE}
+  controller
+>
+  <StaticMap
+    reuseMaps
+    mapStyle='mapbox://styles/mapbox/dark-v10'
+    mapboxApiAccessToken={mapboxToken}
+    preventStyleDiffing
+  />
+</DeckGL>
+
 export const renderToDOM = container => {
   csv(csvFile, (error, data) => {
     if (error) {
       console.error(error)
       return
     }
-    render(<Heatmap data={data} />, container)
+    render(<ScreenGrid data={data} />, container)
   })
 }
