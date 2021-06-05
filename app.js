624decb121fe7695f4f17b5f6e99eac41163a55d
Clemens Anzmann <clemens.anzmann@googlemail.com>
2021-06-06 00:13:25 +0100

    aggregate data and style heatmap

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DeckGL from '@deck.gl/react'
 import { render } from 'react-dom'
 import { StaticMap } from 'react-map-gl'
-import { HeatmapLayer, ScreenGridLayer } from '@deck.gl/aggregation-layers'
+import { HeatmapLayer } from '@deck.gl/aggregation-layers'
 import { csv } from 'd3-request'
 import { mapboxToken } from './creds.js'
 import csvFile from './data/array.csv'
@@ -10,47 +10,35 @@ import csvFile from './data/array.csv'
 const INITIAL_VIEW_STATE = {
   longitude: -0.1,
   latitude: 51.5,
-  zoom: 7.5,
-  minZoom: 0,
-  maxZoom: 15,
+  zoom: 11,
+  minZoom: 10,
+  maxZoom: 13,
   pitch: 0,
   bearing: 0
 }
 
-const Heatmap =({ data }) => <DeckGL
+const colorRange = [
+  [5, 255, 8, 20],
+  [5, 217, 8, 50],
+  [154, 178, 76, 100],
+  [253, 141, 60, 150],
+  [240, 59, 32, 182],
+  [189, 0, 38, 200]
+]
+
+const Heatmap = ({ data }) => <DeckGL
   layers={[new HeatmapLayer({
     id: 'heatmap',
     data: data,
     getPosition: d => {
       return [Number(d.lon), Number(d.lat)]
     },
+    colorRange,
     getWeight: d => Number(d.conc),
     aggregation: 'MEAN',
-    radiusPixels: 30,
+    radiusPixels: 50,
     intensity: 1,
-    threshold: 0.05
-  })]}
-  initialViewState={INITIAL_VIEW_STATE}
-  controller
->
-  <StaticMap
-    reuseMaps
-    mapStyle='mapbox://styles/mapbox/dark-v10'
-    mapboxApiAccessToken={mapboxToken}
-    preventStyleDiffing
-  />
-</DeckGL>
-
-const ScreenGrid =({ data }) => <DeckGL
-  layers={[new ScreenGridLayer({
-      id: 'grid',
-      data,
-      opacity: 0.8,
-      getPosition: d => [Number(d.lon), Number(d.lat)],
-      getWeight: d => Number(d.conc),
-      cellSizePixels: 10,
-      gpuAggregation: true,
-      aggregation: 'MEAN'
+    threshold: 0.03
   })]}
   initialViewState={INITIAL_VIEW_STATE}
   controller
@@ -69,6 +57,6 @@ export const renderToDOM = container => {
       console.error(error)
       return
     }
-    render(<ScreenGrid data={data} />, container)
+    render(<Heatmap data={data} />, container)
   })
 }
--- script.js
+++ script.js
@@ -3,17 +3,32 @@ const parse = require('csv-parse')
 const fs = require('fs') 
 
 const data = []
+const aggregatedMap = {}
+const FLOATING_POINT = 4
+
 fs.createReadStream('./data/no2.csv')
   .pipe(parse({ delimiter: ',', from: 2 }))
   .on('data', row => {
     const point = new OsGridRef(Number(row[0]), Number(row[1]));
     const latlon = OsGridRef.osGridToLatLong(point);
-    data.push([ latlon._lat, latlon._lon, Number(row[2])]);
+    const dataValue = Number(row[2])
+    data.push([ latlon._lat, latlon._lon, dataValue]);
+
+    const posKey = `${latlon._lat.toFixed(FLOATING_POINT)}-${latlon._lon.toFixed(FLOATING_POINT)}`
+    if (!aggregatedMap[posKey]) {
+      aggregatedMap[posKey] = { position: [latlon._lat.toFixed(FLOATING_POINT), latlon._lon.toFixed(FLOATING_POINT)], values: [] }
+    }
+    aggregatedMap[posKey].values.push(dataValue)
   })
   .on('end', () => {
+    const aggDataArray = Object.values(aggregatedMap).map(v => {
+      const sum = v.values.reduce((a, b) => a + b, 0);
+      const avg = (sum / v.values.length) || 0;
+      return [ ...v.position, avg]
+    })
     const file = fs.createWriteStream('./data/array.csv');
     file.on('error', err => { console.error(err) });
     file.write('lat,lon,conc\n')
-    data.forEach(row => { file.write(row.join(',') + '\n'); });
+    aggDataArray.forEach(row => { file.write(row.join(',') + '\n'); });
     file.end();
   })
